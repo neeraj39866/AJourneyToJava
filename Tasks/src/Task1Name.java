f01a6764a5aaea8c057b397ac7fcf488e3f6e81b
neera <neera@DESKTOP-MG7APQT>
2020-08-13 10:18:49 +0530

    Session1 and Task Files

---
+++ Tasks/src/Task1Name.java
@@ -0,0 +1,27 @@
+
+
+public class Task1Name {
+
+	public static void main(String[] args) {
+		char ch = '\u0928';
+		char c='\u0940';
+		char d='\u0930';
+		char e= '\u091C';
+
+	    char ch1 = '\u0A28';
+		char c1='\u0A35';
+		char d1='\u0A1C';
+		char e1= '\u0A40';
+		char e2='\u0A24';
+		System.out.print(ch);
+		System.out.print(c);
+		System.out.print(d);
+		System.out.print(e);
+		System.out.println("");
+		System.out.print(ch1);
+		System.out.print(c1);
+		System.out.print(d1);
+		System.out.print(e1);
+		System.out.print(e2);
+	}
+}
---
+++ Tasks/src/Task2Shift.java
@@ -0,0 +1,16 @@
+
+public class Task2Shift {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num3 = -8; 
+		int num5= 8;
+		// 1 0 0 0
+		int num4 = num3 >> 2;		// _ _ 1 0 -> 0 0 1 0 -> 2 | divide the number by 2 power shift
+		int num6=num5>>2;
+		System.out.println("num4 is: "+num4);
+		System.out.println("num6 is: "+num6);
+
+	}
+
+}
